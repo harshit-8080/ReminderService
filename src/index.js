9d5dfa43333d5385a6dad75b9f7ccc6ffac44f3f
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-01-03 03:36:39 +0530

    adding nodemailer config and test send mails

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
-
 const bodyParser = require("body-parser");
 const express = require("express");
 const {PORT} = require("./config/server.config.js");
 const app = express();
+const sendMail = require("./utils/sendMail");
 
 
 const setupAndStartServer = async (req, res) => {
@@ -15,6 +14,15 @@ const setupAndStartServer = async (req, res) => {
     app.listen(PORT, (req, res) => {
         console.clear();
         console.log(`Server started at ${PORT}`);
+
+        sendMail(
+            
+            "airlinebooking12@gmail.com",
+            "harshitrajlnctcse@gmail.com",
+            "Testing Mail",
+            "Hey This is Body Test"
+
+        )
     })
 
 }
