1659685886a1a936a7bb13f4faf390b41e9efd6d
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-01-22 03:50:51 +0530

    adding message broker

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -2,9 +2,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    
-    PORT:process.env.PORT,
-    EMAIL_ID:process.env.EMAIL_ID,
-    EMAIL_PASS:process.env.EMAIL_PASS
-
-}
+  PORT: process.env.PORT,
+  EMAIL_ID: process.env.EMAIL_ID,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  BROKER_URL: process.env.BROKER_URL,
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+  QUEUE_NAME: process.env.QUEUE_NAME,
+  REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY,
+};
---
+++ src/utils/channel.js
@@ -0,0 +1,49 @@
+const amqplib = require("amqplib");
+const {
+  BROKER_URL,
+  EXCHANGE_NAME,
+  QUEUE_NAME,
+} = require("../config/server.config");
+
+const createChannel = async () => {
+  try {
+    const connection = await amqplib.connect(BROKER_URL);
+    const channel = await connection.createChannel();
+
+    // ! exchange broker (distribute to different channels queue) using binding keys
+    await channel.assertExchange(EXCHANGE_NAME, "direct", false);
+
+    return channel;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const publish = async (channel, binding_key, message) => {
+  try {
+    // ! assertQueue will check if given queue is there or not, it not it'll creates a new queue
+    await channel.assertQueue(QUEUE_NAME);
+    await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
+  } catch (error) {}
+};
+
+const subscribe = async (channel, binding_key, service) => {
+  try {
+    const applicationQueue = await channel.assertQueue(QUEUE_NAME);
+    channel.bindQueue(applicationQueue.queue, (msg) => {
+      console.log("received data");
+      console.log(msg.content.toString());
+      channel.ack(msg);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  createChannel,
+  publish,
+  subscribe,
+};
+
+//  http://localhost:15672 ==> rabbitmq url
